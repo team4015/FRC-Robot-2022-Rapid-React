1d2e858bdccd131c0b19a9a9576c8e7901f6414f
bricktrooper <kylepinto@yahoo.ca>
2022-02-05 22:05:10 -0500

    Add templates for subsystems

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,45 @@
+/* ==================================================
+ * Authors:
+ *
+ * --------------------------------------------------
+ * Description:
+ *
+ * ================================================== */
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase
+{
+  // HARDWARE //
+
+  // declare motor controllers, solenoids, and other hardware here
+
+  // CONSTANTS //
+
+
+
+  // CONSTRUCTOR //
+
+  public Shooter()
+  {
+    // instantiate hardware
+  }
+
+  // METHODS //
+
+  // This method will be called once per scheduler run
+  @Override
+  public void periodic()
+  {
+
+  }
+
+  // This method will be called once per scheduler run during simulation
+  @Override
+  public void simulationPeriodic()
+  {
+
+  }
+}
