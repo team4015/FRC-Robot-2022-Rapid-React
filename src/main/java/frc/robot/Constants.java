bd8bfb0930ede0ce553b3f842a639b4b982192ea
Geotricity <72613772+Geotricity@users.noreply.github.com>
2022-02-15 15:13:27 -0500

    Update Constants.java
    - Added Controller Port number
    - Added constants to control robot movement

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,27 @@
+/* ==================================================
+Authour: Shane Pinto & Lucas Jacobs
+Description: Constants.java contains most of the 
+constant values used throughout the robot. This 
+includes settings, hardware ports, and other values.
+================================================== */
+
+package frc.robot;
+
+public final class Constants
+{
+    // HARDWARE PORTS
+   
+
+    // CONTROLLER COMPUTER PORTS
+    public static final int DUALSHOCK = 0;
+
+    // LIMIT SWITCH PORTS
+
+
+    // SETTINGS
+    public static final double DEADZONE = 0.15; // Deadzone applied to joysticks to aid in adjusting sensitivity
+    public static final double MAX_THROTTLE_SPEED = 0.30;
+    public static final double MAX_STEER_SPEED = 0.2;
+
+    // TARGETING CONSTANTS
+}
