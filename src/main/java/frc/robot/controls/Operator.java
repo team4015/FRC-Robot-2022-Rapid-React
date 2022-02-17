a18d92a01b21ac4508d445d616de11f19ed83af8
Geotricity <72613772+Geotricity@users.noreply.github.com>
2022-02-17 15:18:29 -0500

    Added Intake Commands
     - Added spin, reverse, deploy, retract, and stop methods for the intake
    - Added buttons to Operator to run the intake commands
    - Added stop as the default intake command

--- src/main/java/frc/robot/controls/Operator.java
+++ src/main/java/frc/robot/controls/Operator.java
@@ -1,5 +1,5 @@
 /* ==================================================
- * Authors:
+ * Authors: Lucas Jacobs
  *
  * --------------------------------------------------
  * Description:
@@ -13,29 +13,52 @@
 
 package frc.robot.controls;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.*;
+import frc.robot.commands.intake.*;
 import frc.robot.Robot;
 
 public class Operator
 {
     // JOYSTICKS / CONTROLLERS //
-
-
+    Joystick dualshock;
 
     // BUTTONS //
+    private JoystickButton intakeIn;
+    private JoystickButton intakeOut;
 
+    private JoystickButton intakeDeploy;
+    private JoystickButton intakeRetract;
 
 
 	// CONSTANTS //
+    public static final int DUALSHOCK = 2; 
 
+    public static final int INTAKE_IN = 8;
+    public static final int INTAKE_OUT = 6;
+
+    public static final int INTAKE_DEPLOY = 10;
+    public static final int INTAKE_RETRACT = 9;
 
 
     public Operator(Robot robot)
     {
 		// instantiate joysticks / controllers
+        dualshock = new Joystick(DUALSHOCK);
 
         // bind button objects to physical buttons
+        intakeIn = new JoystickButton(dualshock, INTAKE_IN);
+        intakeOut = new JoystickButton(dualshock, INTAKE_OUT);
+
+        intakeDeploy = new JoystickButton(dualshock, INTAKE_DEPLOY);
+        intakeRetract = new JoystickButton(dualshock, INTAKE_RETRACT); 
 
         // bind buttons to commands
+        intakeIn.whenHeld(new IntakeSpin(robot));
+        intakeOut.whenHeld(new IntakeReverse(robot));
+
+        intakeDeploy.whenPressed(new IntakeDeploy(robot));
+        intakeRetract.whenPressed(new IntakeRetract(robot));
     }
 
 	// METHODS //
