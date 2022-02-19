e64573be6a24fe523e4c5e2cde35596d7f4c3aa0
LegenWaitForIt-Dary <Wallsallw@gmail.com>
2022-02-19 12:49:13 -0500

    Update Climber.java
    edits to climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,7 +2,8 @@
  * Authors: Jason and Alexandra
  *
  * --------------------------------------------------
- * Description:
+ * Description: 
+ * Contains the functions to control the climber.
  *
  * ================================================== */
 
@@ -13,16 +14,13 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Climber extends SubsystemBase
 {
+  // HARDWARE //
   private Spark spoolmotor;
   private Spark gearmotor;
-  
-  // HARDWARE //
 
   // Ports //
   public static final int CLIMBER_SPARK_SPOOL = -1;
   public static final int CLIMBER_SPARK_GEAR = -1;
-
-  // declare motor controllers, solenoids, and other hardware here
 
   // CONSTANTS //
 
@@ -31,28 +29,34 @@ public class Climber extends SubsystemBase
   public static final double GEAR_SPEED = 0.1;
   public static final double GEAR_REVERSE_SPEED = -0.1;
 
-  // CONSTRUCTOR //
-
   public Climber()
   {
     // instantiate hardware
     spoolmotor = new Spark(CLIMBER_SPARK_SPOOL);
+    //the climber is run by a spool and gear motor, controlled by two Spark motor controllers.
     gearmotor = new Spark(CLIMBER_SPARK_GEAR);
   }
 
   // METHODS //
+  //unwind() winds out cabel and strap to climb
+public void unwind(){
+    spoolmotor.set(SPOOL_REVERSE_SPEED);
+  }
+  //extend() unwinds cabel, and extends the extension rod
 public void extend(){
-  spoolmotor.set(SPOOL_REVERSE_SPEED);
+  unwind();
   gearmotor.set(GEAR_SPEED);
 }
-public void unextend(){
+//retract() retracts the rod, after the cabel has been unwinded.
+public void retract(){
   spoolmotor.set(0);
   gearmotor.set(GEAR_REVERSE_SPEED);
 }
+//climb() after the rod has been retracted, the spool winds in the cabel, so the robot can climb.
 public void climb(){
   spoolmotor.set(SPOOL_SPEED);
   gearmotor.set(0);
-}
+}//stop() allows both motors to immedediatly stop spinning 
 public void stop(){
   gearmotor.set(0);
   spoolmotor.set(0);
