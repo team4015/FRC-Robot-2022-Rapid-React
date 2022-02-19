23566b88e333a09139ea89fecda4d54881b8aeb7
Jason Wang <91734286+LegenWaitForIt-Dary@users.noreply.github.com>
2022-02-19 13:03:08 -0500

    Climber Subsystem (#19)
    * Update Climber.java
    
    * Update Climber.java
    
    edits to climber subsystem
    
    * Update Climber.java
    
    fixed typos

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,33 +1,65 @@
-/* ==================================================
- * Authors:
+ /* ==================================================
+ * Authors: Jason and Alexandra
  *
  * --------------------------------------------------
- * Description:
+ * Description: 
+ * Contains the functions to control the climber.
  *
  * ================================================== */
 
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Climber extends SubsystemBase
 {
   // HARDWARE //
+  private Spark spoolmotor;
+  private Spark gearmotor;
 
-  // declare motor controllers, solenoids, and other hardware here
+  // Ports //
+  public static final int CLIMBER_SPARK_SPOOL = -1;
+  public static final int CLIMBER_SPARK_GEAR = -1;
 
   // CONSTANTS //
 
-
-
-  // CONSTRUCTOR //
+  public static final double SPOOL_SPEED = 0.1;
+  public static final double SPOOL_REVERSE_SPEED = -0.1;
+  public static final double GEAR_SPEED = 0.1;
+  public static final double GEAR_REVERSE_SPEED = -0.1;
 
   public Climber()
   {
     // instantiate hardware
+    spoolmotor = new Spark(CLIMBER_SPARK_SPOOL);
+    //the climber is run by a spool and gear motor, controlled by two Spark motor controllers.
+    gearmotor = new Spark(CLIMBER_SPARK_GEAR);
   }
 
   // METHODS //
-
+  //unwind() winds out cable and strap to climb
+public void unwind(){
+    spoolmotor.set(SPOOL_REVERSE_SPEED);
+  }
+  //extend() unwinds cable, and extends the extension rod
+public void extend(){
+  unwind();
+  gearmotor.set(GEAR_SPEED);
+}
+//retract() retracts the rod, after the cable has been unwinded.
+public void retract(){
+  spoolmotor.set(0);
+  gearmotor.set(GEAR_REVERSE_SPEED);
+}
+//climb() after the rod has been retracted, the spool winds in the cable, so the robot can climb.
+public void climb(){
+  spoolmotor.set(SPOOL_SPEED);
+  gearmotor.set(0);
+}//stop() allows both motors to immedediatly stop spinning 
+public void stop(){
+  gearmotor.set(0);
+  spoolmotor.set(0);
+}
 
 }
