0848fe571486f56d2c25cb244178616a34ede76c
Geotricity <72613772+Geotricity@users.noreply.github.com>
2022-03-01 14:39:22 -0500

    Fix Suggestions
    - Finds the biggest contour instead of the first
    - Outputs camera to drive station
    - Added threshold to auto aim

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -9,10 +9,15 @@
 
 package frc.robot.subsystems;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.vision.VisionThread;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -23,9 +28,11 @@ public class Vision extends SubsystemBase {
   // PORTS //
 
   // CONSTANTS //
-  final static int HEIGHT = 120;
-  final static int WIDTH = 160;
+  final static int HEIGHT = 480;
+  final static int WIDTH = 640;
   final static int FPS = 30;
+
+  final static int TURN_THRESHOLD = 20;
 
   // VARIABLES //
   private VisionThread visionThread;
@@ -48,22 +55,38 @@ public class Vision extends SubsystemBase {
     // initalize camera
     UsbCamera cam = CameraServer.startAutomaticCapture();
 
+    CvSink vIn = CameraServer.getVideo();
+    CvSource vOut = CameraServer.putVideo("VideoOutput", 640, 480);
+
     cam.setResolution(WIDTH, HEIGHT);
     cam.setFPS(FPS);
 
     // initalize vision thread
     visionThread = new VisionThread(cam, new GripPipeline(), pipeline -> {
       
-      if (!pipeline.filterContoursOutput().isEmpty()) {
-        Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0)); //Gets bounding rectangle around the target
-        synchronized (imgLock) {
-          xCentre = r.x + (r.width / 2); //Set the centre of the bounding rectangle
+      //Create output frames which will have rectangles drawn on them
+      Mat output = new Mat();
+      vIn.grabFrame(output);
 
-          /* Resize xCentre so that it is in the original resolution of the camera
-          * and not the resized resolution in the pipeline */
-          xCentre /= WIDTH/pipeline.filterContoursOutput().get(0).cols();
+      if (!pipeline.filterContoursOutput().isEmpty()) {
+
+        //Get biggest contour and find its centre
+
+        Rect biggest = new Rect(); // Initialized to 0 area rectangle
+
+        for (int i = 0; i < pipeline.filterContoursOutput().size(); i++) {
+          Rect contour = Imgproc.boundingRect(pipeline.filterContoursOutput().get(i));
+
+          if (contour.area() > biggest.area()) biggest = contour;
+
+          Imgproc.rectangle(output, contour, new Scalar(0, 255, 0, 255), 1); // Add rectangle to the output
+        }
+        
+        synchronized (imgLock) {
+          xCentre = biggest.x + (biggest.width / 2); //Set the centre of the bounding rectangle
         }
       }
+      vOut.putFrame(output);
     });
 
     visionThread.start();
@@ -81,6 +104,12 @@ public class Vision extends SubsystemBase {
     synchronized (imgLock) {
       xCentre = this.xCentre;
     }
-    return xCentre - (WIDTH/ 2); // return difference between the target and where the robot is pointed
+
+    double turn = xCentre - (WIDTH/ 2);
+
+    // If the robot is within the turn threshold of pointing straight at the target, it wil stop turning
+    if (Math.abs(turn) < TURN_THRESHOLD) turn = 0;
+
+    return turn; // return difference between the target and where the robot is pointed
   }
 }
