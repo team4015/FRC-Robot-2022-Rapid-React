0e1e7fd02386b14b3598d94e3da86abd06abd19c
Geotricity <72613772+Geotricity@users.noreply.github.com>
2022-04-05 19:46:32 -0400

    encoder test

--- src/main/java/frc/robot/Match.java
+++ src/main/java/frc/robot/Match.java
@@ -13,6 +13,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -37,6 +38,9 @@ public class Match extends TimedRobot
 
     //Put Scheduler on Dashboard
     SmartDashboard.putData(CommandScheduler.getInstance());
+
+    // Disable Live Window that was causing loop overruns
+    //LiveWindow.disableAllTelemetry();
 
     //Create menu for commands that run in autonomous
     autoMode = new SendableChooser<>();
