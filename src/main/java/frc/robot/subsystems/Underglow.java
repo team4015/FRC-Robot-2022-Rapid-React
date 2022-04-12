39ea2416c503f9d869084709789424d09bc1d0cd
bricktrooper <kylepinto@yahoo.ca>
2022-04-11 21:32:34 -0400

    Asynchronous TX pacing

--- src/main/java/frc/robot/subsystems/Underglow.java
+++ src/main/java/frc/robot/subsystems/Underglow.java
@@ -4,71 +4,93 @@
  * --------------------------------------------------
  * Description: Code for LED Light strips underneath
  * robot. Communicates via Serial using the RS-232
- * port on the Roborio.
- * 
+ * port (UART) on the RoboRIO.  Bytes will only be
+ * transmitted to the RGB controller if busy()
+ * returns false, meaning that there are no pending
+ * bytes to send.
+ *
  * RGB Controller Repository: https://github.com/team4015/RGB-Controller
  * ================================================== */
 
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Underglow extends SubsystemBase
 {
-  SerialPort uart;
-  
-  final int HEADER_BYTE = 0xFF;
-  final int BAUD_RATE = 9600;
-  final double TIMEOUT = 0.1; // 100 ms
-  final double TRANSMIT_DELAY = 0.01; // 10 ms
+  private static final int HEADER = 0xFF;
+  private static final int BAUD_RATE = 9600;
+  private static final double TIMEOUT = 100e-3;    // 100 ms
+  private static final double TX_DELAY = 1.5e-3;   // 1.5 ms
+
+  private SerialPort uart;
+  private Timer timer;       // tracks the time between transmitted bytes
+  private int index;         // index of the next byte to be transmitted
+  private int [] data;       // data to be transmitted
 
   public Underglow()
   {
-    uart = new SerialPort(BAUD_RATE, Port.kOnboard);
-
+    uart = new SerialPort(BAUD_RATE, SerialPort.Port.kOnboard);
     uart.setTimeout(TIMEOUT);
+
+    timer = new Timer();
+    timer.reset();
+    timer.start();
+
+    index = 0;
+    data = new int[0];
   }
 
-  public void transmitByte(int value)
+  public boolean busy()
   {
-    byte [] buffer = {(byte) value};
-    uart.write(buffer, buffer.length);
-    uart.flush();
-    Timer.delay(TRANSMIT_DELAY);
+    return index < data.length;
   }
 
-  public void transmit(int [] data)
+  @Override
+  public void periodic()
   {
-    for (int i = 0; i < data.length; i++)
+    if (busy() && timer.get() >= TX_DELAY)
     {
-      if (data[i] > 255 || data[i] < 0)
-      {
-        return;
-      }
+      byte [] buffer = new byte[1];
+      buffer[0] = (byte)data[index];
+
+      uart.write(buffer, 1);
+      uart.flush();
+
+      timer.reset();
     }
+  }
 
-    for (int i = 0; i < data.length; i++)
+  private void transmit(int [] data)
+  {
+    if (!busy())
     {
-      transmitByte(data[i]);
+      this.data = data;
+      index = 0;
     }
   }
 
   public void setColour(int red, int green, int blue)
   {
-    int [] colourPacket = {HEADER_BYTE, red, green, blue};
+    int [] colourPacket = new int[4];
+
+    colourPacket[0] = HEADER;
+    colourPacket[1] = red;
+    colourPacket[2] = green;
+    colourPacket[3] = blue;
+
     transmit(colourPacket);
   }
 
-  // COLOURS
+  // COLOURS //
 
   public void off()
   {
     setColour(0, 0, 0);
   }
-  
+
   public void blue()
   {
     setColour(0, 0, 255);
